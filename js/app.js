a50054b0d54eff1d61a2f5836eccd9b88affeb1b
06011-39-03 <06011-39-03@exchange.ad.afpanet>
2024-11-18 10:30:36 +0100

    add btns taxe

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let tvaValue = document.getElementById('montantTVA');
 let ttcValue = document.getElementById('montantTTC');
 
 
-let currentTva  = 20/100;
+let currentTva  = 20;
 
 let mhtValueCurent ;
 let tvaValueCurent ;
@@ -14,49 +14,55 @@ let ttcValueCurent ;
 
 
 
-console.log(inputTva)
+ 
+
+   listInputs.forEach(inp => {
+      inp.addEventListener('click', function(){
+         currentTva = parseFloat(inp.value.replace("%","")); 
+         inputTva.value = currentTva;
+         tvaValueCurent = mhtValueCurent * (currentTva/100) ;
+        tvaValue.value = tvaValueCurent;
+        ttcValueCurent = tvaValueCurent + mhtValueCurent;
+        ttcValue.value = ttcValueCurent;
+      })        
+     });
+
+
+   inputTva.addEventListener("blur", function(){
+      currentTva = inputTva.value
+      tvaValueCurent = mhtValueCurent * (currentTva/100) ;
+      tvaValue.value = tvaValueCurent;
+      ttcValueCurent = tvaValueCurent + mhtValueCurent;
+      ttcValue.value = ttcValueCurent;
+   });
+
+  
+  
+   mhtValue.addEventListener("blur", function(){
+      mhtValueCurent = parseFloat(mhtValue.value.replace("€", ""))
+      tvaValueCurent = mhtValueCurent * (currentTva/100) ;
+      tvaValue.value = tvaValueCurent;
+      ttcValueCurent = tvaValueCurent + mhtValueCurent;
+      ttcValue.value = ttcValueCurent;
+   });
+  
+   tvaValue.addEventListener("blur", function(){
+      tvaValueCurent = parseFloat(tvaValue.value.replace("€", ""));
+      mhtValueCurent = tvaValueCurent/(currentTva/100);
+      mhtValue.value = mhtValueCurent;
+      ttcValueCurent = mhtValueCurent + tvaValueCurent;
+      ttcValue.value = ttcValueCurent;
+   });
+  
+   ttcValue.addEventListener("blur", function(){
+      ttcValueCurent = parseFloat(ttcValue.value.replace("€", ""));
+      mhtValueCurent = ttcValueCurent/(1+(currentTva/100));
+      mhtValue.value = mhtValueCurent;
+      tvaValueCurent = ttcValueCurent - mhtValueCurent;
+      tvaValue.value = tvaValueCurent;
+   })
 
  
 
 
-listInputs.forEach(inp => {
- inp.addEventListener('click', function(){
-    currentTva = inp.value; 
-    inputTva.value = currentTva
-    console.log(currentTva)
- })
-    
-});
 
-inputTva.addEventListener("blur", function(){
-    currentTva = inputTva.value
-
- });
-
- mhtValue.addEventListener("blur", function(){
-    mhtValueCurent = parseFloat(mhtValue.value.replace("€", ""))
-    tvaValueCurent = mhtValueCurent * currentTva ;
-    tvaValue.value = tvaValueCurent;
-    ttcValueCurent = tvaValueCurent + mhtValueCurent;
-    ttcValue.value = ttcValueCurent
- });
-
- tvaValue.addEventListener("blur", function(){
-    tvaValueCurent = parseFloat(tvaValue.value.replace("€", ""));
-    mhtValueCurent = tvaValueCurent/currentTva;
-    mhtValue.value = mhtValueCurent;
-    ttcValueCurent = mhtValueCurent + tvaValueCurent;
-    ttcValue.value = ttcValueCurent;
- });
-
- ttcValue.addEventListener("blur", function(){
-    ttcValueCurent = parseFloat(ttcValue.value.replace("€", ""));
-    mhtValueCurent = ttcValueCurent/(1+currentTva);
-    mhtValue.value = mhtValueCurent;
-    tvaValueCurent = ttcValueCurent - mhtValueCurent;
-    tvaValue.value = tvaValueCurent;
- })
-
- function calculator (){
-
- }
